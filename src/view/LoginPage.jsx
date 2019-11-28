ab0adfe2182f78880aa2654098f91ff7cbd214ba
alabo <festusalabo@gmail.com>
2019-11-28 14:05:56 +0100

    Added login and register

--- src/view/LoginPage.jsx
+++ src/view/LoginPage.jsx
@@ -1,22 +1,13 @@
 import React, { Component, Fragment } from "react";
-import About from "../components/about/about";
-import Features from "../components/features/features";
-import Projects from "../components/projects/projects";
-import Steps from "../components/steps/steps";
-import Roadmap from "../components/roadMap/roadmap";
-import Tokens from "../components/token/tokens";
-import PressMedia from "../components/pressmedia/pressmedia";
+import Login from "./login";
 import Subscribe from "../components/sub/subscribe";
-import MainSlider from "../components/mainSlider/mainSlider";
-import Currency from "../components/currency/currency";
-import Teams from "../components/team/team";
-import Blogs from "../components/blog/blog";
+
 
 class LoginPage extends Component {
 	render() {
 		return (
 			<Fragment>
-				<Tokens />
+				<Login />
 
 				<Subscribe />
 			</Fragment>
---
+++ src/view/register.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import { Container, Row, Col, Button } from 'reactstrap';
+
+class Tokens extends Component {
+  render() {
+    let contents = (
+      <Row>
+            <br></br>
+            <br></br>
+        <Col lg={12} md={12} sm={12} className="col-xs-12">
+          <div className="section-heading2">
+            <h2>Register Page</h2>
+          </div>
+        </Col>
+        <Col lg={10} md={12} sm={12} className="col-xs-12 col-lg-offset-1">
+          <Col lg={6} md={6} sm={6} className="col-xs-12">
+            <div className="tokens">
+              <div className="token-body">
+              <div className="token-name">REGISTER</div>
+                  
+            <form id="register-form" class="form-box form-ajax">
+              <div className="form-group">
+                <label for="firstName">First Name</label>
+                <input type="text" name="firstName" id="firstName" className="form-control form-value"
+                  placeholder="First Name" />
+              </div>
+              <div className="form-group">
+                <label for="lastName">Last Name</label>
+                <input type="text" name="lastName" id="lastName" className="form-control form-value" placeholder="Last Name"
+                  />
+              </div>
+              <div className="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" className="form-control form-value" 
+                  placeholder="Email" />
+              </div>
+              <div className="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" className="form-control form-value"
+                  placeholder="Password" />
+              </div>
+              <Button  type="submit" className="btn3 mt-3" variant="dark">   
+                  Register
+              </Button>
+            </form>
+            <p className="text-center">Already have an account? <a href="/login" className="para">Login</a></p>
+            <p className="text-center">By registering, you agree to our <a href="#" className="para" target="_blank"
+                rel="noopener noreferrer">Terms and Conditions</a></p>
+        
+            </div>
+            </div>
+          
+            </Col>
+        </Col>
+      </Row>
+    )
+    
+    return (
+      <div id="tokens" className="wd_scroll">
+        <section className="tokens-area section">
+          <Container>
+            {contents}
+          </Container>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Tokens;

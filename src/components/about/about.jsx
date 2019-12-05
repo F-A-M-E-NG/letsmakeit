aeab042a0db8c50ef49cb069ec3cd003d1f36013
alabo <festusalabo@gmail.com>
2019-12-05 16:49:10 +0100

    Changed about content

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -10,24 +10,19 @@ class About extends Component {
 			<Row style={{ paddingTop: "10vh" }}>
 				<Col lg={6} md={6} sm={12} className="col-xs-12">
 					<div className="about-content">
-						<h2 className="f-40 fw-400">About The Project</h2>
+						<h2 className="f-40 fw-400">About</h2>
 						<p>
-							Bit Money is a lending and investment, multicurrency and
-							multifunctional online platform based on blockchain technology.
-							There investors and borrowers meet each other and have the
-							opportunity to lend money on mutually beneficial.
+						Brass & Books MPCS Ltd is a financial service company dedicated to 
+						providing our customers & members with innovative financial products 
+						and services. We are registered credit union that creates and bridges 
+						business & Income opportunities using a member-centered approach.
 						</p>
 						<p>
-							Bit Money is a unique service that allows individuals to access
-							loans from the comfort of their home in fiat currencies or
-							crypto-currencies.
+						BBMPCS customizes flexible financial solutions tailored to meet 
+						client-members objectives, earning trust through utmost professionalism, 
+						integrity & friendly service
 						</p>
-						<p>
-							Bit Money is a multi-functional platform which allows each
-							participant to keep money in a multi-currency online wallet, buy
-							and sell currency on the exchange, invest money, get a loan in a
-							convenient currency.
-						</p>
+						
 						<div className="buttons">
 							<Link to="/invest" className="btn1">
 								Explore More
@@ -51,31 +46,19 @@ class About extends Component {
 					<Col lg={6} md={6} sm={12} className="col-xs-12">
 						<div className="about-content">
 							<ReactWOW animation={this.props.animate} duration="1s">
-								<h2 className="f-40 fw-400">About The Project</h2>
+								<h2 className="f-40 fw-400">About</h2>
 							</ReactWOW>
 							<ReactWOW animation={this.props.animate} duration="1.3s">
 								<p>
-									Bit Money is a lending and investment, multicurrency and
-									multifunctional online platform based on blockchain
-									technology. There investors and borrowers meet each other and
-									have the opportunity to lend money on mutually beneficial.
+								Brass & Books MPCS Ltd is a financial service company dedicated to providing our customers & members with innovative financial products and services. We are registered credit union that creates and bridges business & Income opportunities using a member-centered approach.
 								</p>
 							</ReactWOW>
 							<ReactWOW animation={this.props.animate} duration="1.6s">
 								<p>
-									Bit Money is a unique service that allows individuals to
-									access loans from the comfort of their home in fiat currencies
-									or crypto-currencies.
+									BBMPCS customizes flexible financial solutions tailored to meet client-members objectives, earning trust through utmost professionalism, integrity & friendly service
 								</p>
 							</ReactWOW>
-							<ReactWOW animation={this.props.animate} duration="1.9s">
-								<p>
-									Bit Money is a multi-functional platform which allows each
-									participant to keep money in a multi-currency online wallet,
-									buy and sell currency on the exchange, invest money, get a
-									loan in a convenient currency.
-								</p>
-							</ReactWOW>
+							
 							<div className="buttons">
 								<Link to="#" className="btn1">
 									WHITEPAPER
